2dd23c7592cef029d6812d399e9a6ab119588a77
Tasin Ishmam <tasinishmam@gmail.com>
2021-04-21 03:06:49 +0600

    First commit
    Create typescript-prisma-graphql boilerplate

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+
+const PORT = 4000;
+const app = express();
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!'
+    },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
+
+app.listen({ port: PORT }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+)
