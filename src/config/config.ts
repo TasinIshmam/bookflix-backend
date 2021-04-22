4c2966b3c8eacb5738ebbea2e5e1d79188412f39
Tasin Ishmam <tasinishmam@gmail.com>
2021-04-23 00:20:10 +0600

    Add config file for loading env

---
+++ src/config/config.ts
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import fs from "fs";
+
+if (fs.existsSync(".env")) {
+    dotenv.config({ path: ".env" })  //load config files
+} else {
+    throw new Error("Could not load .env file.");
+}
+
+
+const env: string = process.env.NODE_ENV || 'development'  //assume development unless otherwise specified
+
+const config = {
+    env,
+    port : process.env.PORT,
+    database: {
+        databaseUrl: process.env.DATABASE_URL,
+    },
+    logger: {
+        logLevel: process.env.LOG_LEVEL,
+    }
+}
+
+export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 import {context} from "./context";
+import config from './config/config';
 
 
-const PORT = 4000;
 const app = express();
 
 const typeDefs = gql`
@@ -21,6 +21,14 @@ const resolvers = {
 const server = new ApolloServer({ typeDefs, resolvers, context});
 server.applyMiddleware({ app });
 
+
+const PORT = config.port || 4000;
+
 app.listen({ port: PORT }, () =>
-    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+    console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`)
 )
+
+process.on('uncaughtException', err => {
+    console.log(`Uncaught Exception: ${err.message}`)
+    process.exit(1)
+})
