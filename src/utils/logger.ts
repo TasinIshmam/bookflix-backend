e2784ea70f515d7448f2229f2bdf0403a69bc591
Tasin Ishmam <tasinishmam@gmail.com>
2021-04-23 00:29:12 +0600

    Add logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,32 @@
+import winston from "winston";
+const {transports, format} = winston;
+import split from "split";
+import config from '../config/config';
+
+
+const print = format.printf((info) => {
+    const log = `${info.level}: ${info.message}`;
+
+    return info.stack
+        ? `${log}\n${info.stack}`
+        : log;
+});
+
+let logLevelConsole = config.logger.logLevel;
+
+const logger = winston.createLogger({
+    level: logLevelConsole,
+    format: format.combine(
+        format.errors({stack: true}),
+        print,
+    ),
+    transports: [new transports.Console()],
+});
+
+logger.stream = split().on('data', function (line) {
+    logger.info(line)
+});
+
+
+export default logger;
+
