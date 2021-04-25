44d22300af7e8f555c289888241c7373c6f27fd7
Tasin Ishmam <tasinishmam@gmail.com>
2021-04-25 21:32:24 +0600

    Update config and .env.example

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,15 +2,19 @@ import dotenv from 'dotenv';
 import fs from 'fs';
 
 if (fs.existsSync('.env')) {
-    dotenv.config({ path: '.env' }); //load config files
-} else {
-    throw new Error('Could not load .env file.');
+    dotenv.config({ path: '.env' }); //load environment
 }
 
-const env: string = process.env.NODE_ENV || 'development'; //assume development unless otherwise specified
+if (!process.env.NODE_ENV) {
+    throw new Error("Could not detect NODE_ENV, exiting.");
+}
+
+if (!process.env.DATABASE_URL) {
+    throw new Error("Could not detect DATABASE_URL inside the environment.");
+}
 
 const config = {
-    env,
+    env:  process.env.NODE_ENV,
     port: process.env.PORT,
     database: {
         databaseUrl: process.env.DATABASE_URL,
