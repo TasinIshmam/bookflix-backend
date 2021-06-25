f64aacdce9b58312d3a55c6fbb66c3e9cd7175c0
Tasin Ishmam <tasinishmam@gmail.com>
2021-06-25 07:11:47 +0600

    Fix bug in seeding user data
    Previously password was not being salted when seeding data to db, resulting in invalid password when trying to log in.

--- app/prisma/seed.ts
+++ app/prisma/seed.ts
@@ -1,15 +1,12 @@
 import { PrismaClient, Prisma } from "@prisma/client";
 import * as fs from "fs";
 import * as path from "path";
+import * as bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 const DATA_DIRECTORY = "prisma/seed-data-books-authors-genres";
 
-const SEED_USER: Prisma.UserCreateInput = {
-    username: "seed-user",
-    name: "seed-name",
-    password: "password",
-};
+
 
 function loadSeedData(dataDirectoryPath: string): Prisma.BookCreateInput[] {
     let bookData: Prisma.BookCreateInput[] = [];
@@ -49,6 +46,12 @@ async function main() {
             console.log(`Faulting Data: ${JSON.stringify(e, undefined, 4)}`);
         }
     }
+
+    const SEED_USER: Prisma.UserCreateInput = {
+        username: "seed-user",
+        name: "seed-name",
+        password: await bcrypt.hash("password", 10)
+    };
 
     await prisma.user.create({ data: SEED_USER });
 
