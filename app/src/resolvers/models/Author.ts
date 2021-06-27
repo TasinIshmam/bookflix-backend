256ac52bd0b49ee7b2ba7ce126476ce7df63dfc4
Tasin Ishmam <tasinishmam@gmail.com>
2021-06-27 07:19:37 +0600

    Add isLikedByUser resolver in Author

--- app/src/resolvers/models/Author.ts
+++ app/src/resolvers/models/Author.ts
@@ -1,8 +1,26 @@
-import { Context } from "../../context";
+import { Context, prisma } from "../../context";
 import { Author } from "@prisma/client";
+import { AuthenticationError } from "apollo-server-errors";
 
 export async function books(parent: Author, args, context: Context) {
     return context.prisma.author
         .findUnique({ where: { id: parent.id } })
         .books();
+}
+
+export async function isLikedByUser(parent: Author, args, context: Context) {
+    const { userId } = context;
+    if (!userId) throw new AuthenticationError("Not logged in");
+
+    const userAuthorInteraction = await context.prisma.userAuthorInteraction.findUnique(
+        {
+            where: {
+                authorId_userId: {
+                    authorId: parent.id,
+                    userId: userId
+                }
+            }
+        }
+    )
+    return userAuthorInteraction? userAuthorInteraction.isFavorite : false;
 }
